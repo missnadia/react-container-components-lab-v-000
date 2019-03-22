36c9429f7fb5ee476937ddeebe6d3f48d7ee9cc3
Nadia Lee <miss.nadia.lee@gmail.com>
2019-03-22 03:35:57 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,8 @@ const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+
+export default LatestMovieReviewsContainer extends Component {
+  
+  
+}
